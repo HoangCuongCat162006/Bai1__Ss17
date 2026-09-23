5a5a03cd53490e00e541d37202194c90b9153cd5
Hoang Cuong Cat <cathoangfb1@gmail.com>
2026-09-23 23:01:03 +0700

    Initial commit

---
+++ src/main/java/com/example/bai1__ss17/Bai1Ss17Application.java
@@ -0,0 +1,13 @@
+package com.example.bai1__ss17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bai1Ss17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bai1Ss17Application.class, args);
+    }
+
+}
